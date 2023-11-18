2b8323435969df7a8ba0fe32c296f2fa707a9c3d
İbrahim Halil Demir <151337519+demir1624@users.noreply.github.com>
2023-11-18 12:52:54 +0300

    datetetime

---
+++ datetime.py
@@ -0,0 +1,12 @@
+import datetime
+print("Bugün",datetime.date.today())
+
+from datetime import datetime
+print("şimdi=",datetime.now())
+
+import datetime 
+print("şİmdi=",datetime.datetime.now())
+
+from datetime import date
+
+
